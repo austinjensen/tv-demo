efe2e817544a866f5046eefb10f06dc58e1433de
Austin Jensen <ajjensen1329@gmail.com>
2018-05-02 20:52:02 -0600

    used server data instead inmem data

--- src/App.js
+++ src/App.js
@@ -6,24 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   state = {
-    shows: [
-      {
-        name: 'Walking Dead',
-        rating: 4,
-        image: 'https://pbs.twimg.com/profile_images/956941358219931649/2rsLjOg-_400x400.jpg'
-      },
-      {
-        name: 'Sing',
-        rating: 1,
-        image: 'http://t2.gstatic.com/images?q=tbn:ANd9GcQeTMzh3aGw46IVUdS6N2tToanuLOc9dO7f6CgWVQlq1laJjuXa'
-      },
-      {
-        name: 'The Office',
-        rating: 2,
-        image: 'https://media.gq.com/photos/5a53e9fca929253c4d20a04f/3:2/w_800/does-the-office-hold-up-gq.jpg'
-      }
-    ]
-
+    shows: []
   }
 
   createshow = (show) => {
@@ -36,17 +19,82 @@ class App extends Component {
 
     })
   }
- 
+
+  testPromises = () => {
+    console.log('testing some promises')
+    new Promise((resolve, reject) => {
+      const success = false
+      setTimeout(() => {
+        if (success)
+          resolve('good promise')
+        else
+          reject('bad promise')
+      }, 300)
+
+    })
+      .then((value) => { console.log(value) })
+      .catch((error) => { console.log(error) })
+    console.log('finished project excution')
+  }
+
+  getShows = () => {
+    fetch('http://localhost:3001/shows')
+      .then((response) => {
+        // console.log("response", response)
+        const result = response.json()
+        // console.log("result:", result)
+        return result
+
+      })
+      .then((shows) => {
+        // console.log("jsonData", shows)
+        this.setState({ shows })
+      })
+      .catch((error) => {
+        // console.log(error)
+        this.setState({ errorMessage: error })
+      })
+  }
+  postShow = (showToSave) => {
+    const postInit = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(showToSave)
+    }
+    fetch('http://localhost:3001/shows', postInit)
+      .then((postShowsResp) => {
+        return postShowsResp.json()
+      })
+      .then((show) => {
+        this.createshow(show)
+      })
+      .catch((error) => {
+        this.setState({ errorMessage: error })
+      })
+  }
+  renderError = () => {
+    return this.state.errorMessage
+      ? (<div> {this.state.errorMessage.toString()}</div>)
+      : (<div></div>)
+  }
+
+  componentDidMount() {
+    // this.testPromises()
+    this.getShows()
+  }
+
   render() {
     return (
 
       <Router>
         <div className="App">
-
-
+          {this.renderError()}
           <Switch>
             <Route exact path="/" component={() => <Viewshows allshows={this.state.shows} />} />
-            <Route path="/manageShows" component={() => <Manageshows allshows={this.state.shows} createShow={this.createshow} />} />
+            <Route path="/manageShows" component={() => <Manageshows allshows={this.state.shows} createShow={this.postShow} />} />
           </Switch>
         </div>
       </Router>
